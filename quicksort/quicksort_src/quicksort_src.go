9e2ad6fb29e4b71494f5f1b2b0dcaec406a61ea6
Chiara Mezzavilla <faerie_queene87@yahoo.it>
2017-08-21 20:39:10 +0200

    quicksort-pt-1 first red test

--- quicksort/quicksort_src/quicksort_src.go
+++ quicksort/quicksort_src/quicksort_src.go
@@ -1,1 +1,8 @@
 package quicksort_src
+
+func Partition(unsorted []int) (partitioned []int){
+
+	partitioned = unsorted
+
+	return
+}
--- quicksort/quicksort_src/quicksort_src_test.go
+++ quicksort/quicksort_src/quicksort_src_test.go
@@ -1,1 +1,21 @@
 package quicksort_src
+
+import (
+	"testing"
+	"github.com/samurang87/playground_go"
+)
+
+func TestRegularCase(t *testing.T) {
+
+	input := []int{4, 5, 3, 7, 2}
+
+	want := []int{3, 2, 4, 5, 7}
+
+	got := Partition(input)
+
+	if hackerrank_utils.TestEq(want, got) == false {
+
+		t.Errorf("Wanted %v, got %v", want, got)
+	}
+
+}
