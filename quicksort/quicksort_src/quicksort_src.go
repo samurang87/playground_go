8e57101a673879db09a2b984792ca4b516ea45a1
Chiara Mezzavilla <faerie_queene87@yahoo.it>
2017-08-21 21:11:48 +0200

    quicksort-pt-1 solved riddle

--- quicksort/quicksort_src/quicksort_src.go
+++ quicksort/quicksort_src/quicksort_src.go
@@ -2,7 +2,33 @@ package quicksort_src
 
 func Partition(unsorted []int) (partitioned []int){
 
-	partitioned = unsorted
+	pivot := unsorted[0]
+
+	var left, mid, right []int
+
+	for _, item := range unsorted {
+
+		if item < pivot {
+
+			left = append(left, item)
+
+		} else if item > pivot {
+
+			right = append(right, item)
+
+		} else {
+
+			mid = append(mid, item)
+
+		}
+	}
+
+	partitioned = append(partitioned, left...)
+
+	partitioned = append(partitioned, mid...)
+
+	partitioned = append(partitioned, right...)
 
 	return
+
 }
