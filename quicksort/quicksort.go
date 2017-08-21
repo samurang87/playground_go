20dd6e99796c2c9ac2ef89428c3b311095617a93
Chiara Mezzavilla <faerie_queene87@yahoo.it>
2017-08-21 20:38:44 +0200

    quicksort-pt-1 main method skeleton

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"strings"
+	"fmt"
+	"github.com/samurang87/playground_go"
+)
+
+func main() {
+
+	input := hackerrank_utils.Read()
+
+	for _, item := range input[1:] {
+
+		d := strings.Split(item, " ")
+		fmt.Println(d)
+
+
+	}
+
+}
