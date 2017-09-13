fdacb60b3e002801d6e3082a51a143ed7811ad72
Chiara Mezzavilla <samurang87+apps@gmail.com>
2017-09-13 18:11:19 +0200

    tries-contact test WIP

--- tries_contact/tries_contact_src/tries_contact_src_test.go
+++ tries_contact/tries_contact_src/tries_contact_src_test.go
@@ -1,4 +1,18 @@
 package tries_contact_src
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
+func TestAddSingleWord(t *testing.T){
+
+	var trie Node
+
+	trie.AddEntry("bla")
+
+	for _, child := range(trie.Children) {
+
+		fmt.Println(child.Character)
+	}
+}
