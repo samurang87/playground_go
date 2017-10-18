4d6b28c9f7da1a36f707a6c303e788650197141c
Chiara Mezzavilla <faerie_queene87@yahoo.it>
2017-10-18 21:51:21 +0200

    hash-ransom inner functions with tests work

---
+++ hash_ransom/hash_ransom_src/hash_ransom_src.go
@@ -0,0 +1,44 @@
+package hash_ransom
+
+
+func CreateMap(text_list []string) map[string]int {
+
+	text_map := make(map[string]int)
+
+	for _, word := range text_list {
+
+		if _, ok := text_map[word]; ok {
+
+			text_map[word]++
+
+		} else {
+
+			text_map[word] = 1
+
+		}
+	}
+
+	return text_map
+}
+
+
+func CompareMaps(magazine map[string]int, note map[string]int) (result bool){
+
+	for key, _ := range note {
+
+		if val, ok := magazine[key]; ok {
+
+			if val < note[key] {
+
+				return false
+			}
+
+		} else {
+
+			return false
+		}
+	}
+
+	return true
+
+}
---
+++ hash_ransom/hash_ransom_src/hash_ransom_src_test.go
@@ -0,0 +1,107 @@
+package hash_ransom
+
+import "testing"
+import (
+	"reflect"
+)
+
+func TestCreateMaps(t *testing.T) {
+
+	some_text := []string{"a", "a", "a", "b", "b", "c"}
+
+	text_map := CreateMap(some_text)
+
+	expected_result := make(map[string]int)
+
+	expected_result["a"] = 3
+
+	expected_result["b"] = 2
+
+	expected_result["c"] = 1
+
+	eq := reflect.DeepEqual(expected_result, text_map)
+
+	if eq == false {
+
+		t.Errorf("Not equal! Expected %v, got %v", expected_result, text_map)
+	}
+
+
+}
+
+func TestCompareMapsHappy(t *testing.T) {
+
+	var magazine = map[string]int{
+
+		"a": 3,
+		"b": 2,
+		"c": 1,
+	}
+
+	var note = map[string]int{
+
+		"a": 3,
+		"b": 2,
+
+	}
+
+	result := CompareMaps(magazine, note)
+
+	if result == false {
+
+		t.Errorf("Not equal! Expected %v, got %v", true, result)
+	}
+
+}
+
+func TestCompareMapsKeyMissing(t *testing.T) {
+
+	var magazine = map[string]int{
+
+		"a": 3,
+		"b": 2,
+		"c": 1,
+	}
+
+	var note = map[string]int{
+
+		"a": 3,
+		"b": 2,
+		"d": 3,
+
+	}
+
+	result := CompareMaps(magazine, note)
+
+	if result == true {
+
+		t.Errorf("Not equal! Expected %v, got %v", false, result)
+	}
+
+}
+
+func TestCompareMapsInsufficientNumberOfWords(t *testing.T) {
+
+	var magazine = map[string]int{
+
+		"a": 3,
+		"b": 2,
+		"c": 1,
+	}
+
+	var note = map[string]int{
+
+		"a": 3,
+		"b": 2,
+		"c": 3,
+
+	}
+
+	result := CompareMaps(magazine, note)
+
+	if result == true {
+
+		t.Errorf("Not equal! Expected %v, got %v", false, result)
+	}
+
+}
